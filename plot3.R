5fca873bf10d506eedf35f6f7c54f3d21cccdbf4
Mudit Saxena <mudit02@gmail.com>
2015-05-06 17:40:44 +0530

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,34 @@
+##create a local directory
+if(!file.exists("./data")){dir.create("./data")}
+## download file from given url
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl,destfile="./data/exdata-data-household_power_consumption.zip")
+##unzip the downloaded file
+unzip(zipfile="./data/exdata-data-household_power_consumption.zip",exdir="./data")
+## read complete data from the unzipped file
+completeData<-read.csv("./data/household_power_consumption.txt",
+                       header = TRUE, stringsAsFactors = F,sep=';',
+                       na.strings="?")
+## to check the correctness of imported data take a look at head
+head(completeData)
+##change the format of date variable
+completeData$Date <- as.Date(completeData$Date, format="%d/%m/%Y")
+##create the subset of the completeData
+data <- subset(completeData, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+##remove complete data from memory as it is not required for further task
+rm(completeData)
+##combine date and time column
+datetime <- paste(data$Date, data$Time)
+data$Datetime <- as.POSIXct(datetime)
+##plot the graph
+with(data, {
+  plot(Sub_metering_1~Datetime, type = "l", xlab="", ylab="Energy sub metering")
+  lines(Sub_metering_2~Datetime,col="red")
+  lines(Sub_metering_3~Datetime,col="blue")
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+  })
+##store the plot to a png file
+dev.copy(png, file = "plot3.png", height=480, width=480)
+dev.off()
